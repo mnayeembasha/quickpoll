6f9ac57d235fab5e8f839b5c0f59107c7cf44ad7
mnayeembasha <mnayeembasha@gmail.com>
2025-12-04 10:12:31 +0530

    fix:build errors

--- client/src/components/TeacherDashboard.tsx
+++ client/src/components/TeacherDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SocketContext } from '@/context/SocketContext';
 
 const TeacherDashboard: React.FC = () => {
